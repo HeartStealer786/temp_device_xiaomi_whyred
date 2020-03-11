2a30ac457fb837d24ad9be786edc635d1c3273fc
HeartStealer786 <beingme639@gmail.com>
2020-03-11 20:37:53 +0530

    whyred : adapt for dt2w (3/3)

--- sdm660.mk
+++ sdm660.mk
@@ -347,10 +347,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pixel-libperfmgr
+    android.hardware.power@1.2-service-qti
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
