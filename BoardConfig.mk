12ef0c3d16c2db03e939e99159f7d6cbdd3c39de
HeartStealer786 <beingme639@gmail.com>
2020-04-23 14:20:47 +0530

    whyred: Adapt Defconfig with kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ TARGET_SCREEN_WIDTH := 1080
 #BOARD_HAVE_QCOM_FM := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := whyred-perf_defconfig
+TARGET_KERNEL_CONFIG := whyred_defconfig
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/manifest.xml
